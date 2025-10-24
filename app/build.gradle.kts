ad7d6381e089818c1dcb57f14fdaf2928e389096
ryan <sweetplastik@gmail.com>
2025-10-24 09:48:52 +0900

    Release 1.0.1.00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
 
     // 버전 코드 전략: yyyymmdd + 2자리 시퀀스 (NN)
     // 이전 사용: 2025100800 -> 신규: 2025100801
-    val releaseVersionCode = 2025101400
-    val releaseVersionName = "1.0.0"
+    val releaseVersionCode = 2025102400
+    val releaseVersionName = "1.0.1"
 
     defaultConfig {
         applicationId = "com.sweetapps.nodeliverydiet" // 변경: Play Console에서 com.example.* 금지 대응
